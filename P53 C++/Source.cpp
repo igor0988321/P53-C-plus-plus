36240382caf3fc0bd78d8a1e5a888329b3df7e62
Ruslan <karnaukhruslan6@gmail.com>
2026-05-08 23:58:53 +0300

    Добавьте файлы проекта.

---
+++ P53 C++/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+
+	cout << "Hello World!" << endl;
+	cout << "My name Ruslan" << endl;
+
+}
