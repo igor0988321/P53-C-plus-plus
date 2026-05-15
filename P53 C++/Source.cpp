decc36452d1eeb470ebc0918c833cdd0fa5d6489
Ruslan <karnaukhruslan6@gmail.com>
2026-05-15 22:09:20 +0300

    Lesson 1 | 15.05.2026

--- P53 C++/Source.cpp
+++ P53 C++/Source.cpp
@@ -11,4 +11,61 @@ int main()
 	cout << "Hello World!" << endl;
 	cout << "My name Ruslan" << endl;
 
+	//type name;
+
+	// bool - 1 byte
+
+	// char - 1 byte
+
+	//short - 2 byte
+	//int - 4 byte
+	//long - 4 byte
+	// long long - 8 byte
+
+	//float - 4 byte (7 digits)
+	// double - 8 byte (15 digits)
+	// long double - 16 byte (19 digits)
+
+
+	//char t = 'a';
+	//cout << (char)(t + 3.5) << endl;
+
+	//cout << 5 /3 << endl;
+
+
+	//unsigned int b = 3000000000;
+	// cout << b << enddl;
+
+	// - + ++ --
+	// + - * / % += -= *= /= %=
+	// !
+	// < > <= >= == !=  && ||
+	// () [] {} . , :: ~ ?
+	
+	//int a = 5, b = 4, c;
+	// c = a++ * (--b + a-- / ++b);
+	//cout << a << endl;
+	//cout << b << endl;
+	//cout << c << endl;
+	 
+	// cout << 10 % 5 << endl;
+
+	//a = a + b;
+	// a += b;
+
+	// cout << (int)(15.2 / 4) << endl;
+	
+	//c = !a;
+	//cout << c << endl;
+	
+	//int a, b, c;
+	//cin >> a;
+	//cin >> b;
+	//c = (a + b) / 2;
+	//cout << c << endl;
+
+	//cout << pow(2.2, 3.258) << endl;
+
+
+	return 0;
 }
