41954ea630f4345164e5d8e841d23d62cb6430f6
Ruslan <karnaukhruslan6@gmail.com>
2026-05-17 17:40:34 +0300

    17.05.2026 Home Work1

--- P53 C++/Source.cpp
+++ P53 C++/Source.cpp
@@ -8,8 +8,8 @@ int main()
 	SetConsoleCP(65001);
 	SetConsoleOutputCP(65001);
 
-	cout << "Hello World!" << endl;
-	cout << "My name Ruslan" << endl;
+	//cout << "Hello World!" << endl;
+	//cout << "My name Ruslan" << endl;
 
 	//type name;
 
@@ -66,6 +66,66 @@ int main()
 
 	//cout << pow(2.2, 3.258) << endl;
 
+	//Завдання 1
+	//Користувач вводить із клавіатури час у секундах.Необхідно написати програму, 
+	//яка переведе введені користувачем секунди в години, хвилини, секунди і виведе їх на екран.
+
+
+	//int a, b, c;
+	//cin >> a;
+
+	//b = a / 3600;
+	//a = a % 3600;
+	//c = a / 60;
+	//a = a % 60;
+
+	//cout << "Hour:" << b << endl;
+	//cout << "Minute:" << c << endl;
+	//cout << "Second:" << a << endl;
+
+	//Завдання 2
+	//Написати програму, яка перетворює введене з клавіатури дробове число в грошовий формат.
+	//Наприклад, число 12, 5 має бути перетворено до вигляду 12 грн. 50 коп.
+
+
+	//double money, b, c;
+
+	//cin >> money;
+	//int hrivni = money;
+
+	//b = money - hrivni;
+
+	//c = b * 100;
+	//int kopiyka = c;
+
+	//cout << hrivni << "гривень" << endl;
+	//cout << kopiyka << "копійок" << endl;
+
+
+	//Завдання 3
+	//Написати програму, що обчислює, з якою швидкістю бігун пробіг дистанцію.
+
+	double hour, distance, hour2, speed,  secund, b;
+
+	cout << "Ведіть довжину дистанції (метрів)" << endl;
+	cin >> distance;
+	cout << "Ведіть час (хв.сек)" << endl;
+	cin >> hour;
+
+	int minutes = hour;
+	b = hour - minutes;
+	secund = b * 100;
+
+	hour2 = minutes * 60 + secund;
+
+	speed = (distance / hour2) * 3.6;
+
+	cout << "Обчислення швидкості бігу" << endl;
+	cout << "Ведіть довжину дистанції (метрів) = "<< " " << distance << endl;
+	cout << "Ведіть час (хв.сек)" << hour << endl;
+	cout << "Дистанція:"<< " " << distance << " " << "м" << endl;
+	cout << "Час:"<< " " << minutes << " " << "хв" << " " << " " << secund << " " << "сек" << " " << "=" << " " << hour2 << " " << "сек" << endl;
+	cout << "Ви бігли зі швидкістю"<< " " << speed << " " << " " << "км/год" << endl;
 
 	return 0;
 }
